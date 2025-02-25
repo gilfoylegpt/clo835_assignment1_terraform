3b798f510e532f1510c80a3e6ec7fb42a2e7f007
gilfoylegpt <fuhaojie930@gmail.com>
2025-02-25 16:55:22 -0500

    fix kind sh

--- dev/instances/init_kind.sh
+++ dev/instances/init_kind.sh
@@ -10,5 +10,19 @@
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     rm -f ./kubectl
-    kind create cluster --config kind.yaml
+    
+cat <<EOF > /tmp/kind.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:v1.23.17@sha256:14d0a9a892b943866d7e6be119a06871291c517d279aedb816a4b4bc0ec0a5b3
+  extraPortMappings:
+  - containerPort: 30000
+    hostPort: 30000
+  - containerPort: 30001
+    hostPort: 30001
+EOF
+
+ kind create cluster --config /tmp/kind.yaml
     
