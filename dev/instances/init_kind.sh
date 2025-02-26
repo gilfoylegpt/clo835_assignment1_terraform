7f1a80364857b9ec4c5492e210cc0fb0b625b007
gilfoylegpt <fuhaojie930@gmail.com>
2025-02-25 19:40:39 -0500

    add install git

--- dev/instances/init_kind.sh
+++ dev/instances/init_kind.sh
@@ -2,6 +2,7 @@
     set -ex
     sudo yum update -y
     sudo yum install docker -y
+    sudo yum install git -y 
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
     curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
